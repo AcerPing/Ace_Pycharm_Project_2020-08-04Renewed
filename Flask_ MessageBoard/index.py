0bd7cd5536794d35f79f4fe39a68d6f8771c7457
AcerPing <acer0805@hotmail.com>
2020-11-26 17:44:39 +0800

    20201126

--- Flask_ MessageBoard/index.py
+++ Flask_ MessageBoard/index.py
@@ -1,31 +1,31 @@
-#encoding = uft-8
-from flask import Flask,render_template,request,redirect,escape,Markup
+# encoding = uft-8
+from flask import Flask, render_template, request, redirect, escape, Markup
 from datetime import datetime
 
-#引入MySQL
+# 引入MySQL
 import pymysql
 # import MySQLdb
 
-#申請空間
+# 申請空間
 app = Flask(__name__)
-#127.0.0.1:5000
+# 127.0.0.1:5000
 
 @app.route('/')
 def hello():
-    list = load_data()
-    print(list)
-    #顯示MySQL DataBase完整資訊，方便修正
-    return render_template("index.html",list=list)
+    page = request.args.get('page', default=1, type=int)
+    print("頁數：",page)
+    list = load_data_page(page)
+    print("資料：",list) #顯示MySQL DataBase完整資訊，方便修正
+    return render_template("index.html", list=list)
 
-#方法
-@app.route("/post", methods=['GET','POST'])
+# 方法
+@app.route("/post", methods=['GET', 'POST'])
 def post():
+    # 方法一
+    name = request.args.get("name", "匿名")
+    comment = request.args.get("comment", "暫無留言")
 
-    #方法一
-    name = request.args.get("name","匿名")
-    comment = request.args.get("comment","暫無留言")
-
-    #方法二
+    # 方法二
     # name = request.values.get("name","匿名")
     # comment = request.values.get("comment","暫無留言")
     delete_number = request.values.get("delete_number", None)
@@ -33,7 +33,7 @@ def post():
     modified_name = request.values.get("modified_name", None)
     modified_comment = request.values.get("modified_comment", None)
 
-    #方法三
+    # 方法三
     # name = request.form.get("name","匿名")
     # comment = request.form.get("comment","暫無留言")
 
@@ -43,65 +43,94 @@ def post():
     # print(repr(modified_number))
     # print(repr(delete_number))
 
-    if delete_number != None and delete_number !='':
+    if delete_number != None and delete_number != '':
         delete_message(delete_number)
-    elif modified_number != None and modified_number !='':
+    elif modified_number != None and modified_number != '':
         change_message(modified_number, modified_name, modified_comment)
     else:
         save_time(name, comment, create_time)
 
     return redirect('/')
 
-def save_time(name,comment,create_time):
-    #連接數據庫MySQL
-    conn = pymysql.connect(host="localhost",user="root",db="block",charset="utf8",port=None)
-    cur = conn.cursor() #取得指令操作變數
-    #執行插入方法，入的SQL語句
-    #格式化的作用是為了防止SQL注入，`的作用是為了防止關鍵詞被數據庫解析
-    cur.execute("INSERT INTO `article`(`name`,`comment`,`create_time`) VALUES(%s,%s,%s)",[name,comment,create_time])
+
+def save_time(name, comment, create_time):
+    # 連接數據庫MySQL
+    conn = pymysql.connect(host="localhost", user="root", db="block", charset="utf8", port=None)
+    cur = conn.cursor()  # 取得指令操作變數
+    # 執行插入方法，入的SQL語句
+    # 格式化的作用是為了防止SQL注入，`的作用是為了防止關鍵詞被數據庫解析
+    cur.execute("INSERT INTO `article`(`name`,`comment`,`create_time`) VALUES(%s,%s,%s)", [name, comment, create_time])
     conn.commit()
     conn.close()
+
 
 def delete_message(delete_number):
     # print(repr(delete_number)) #顯示delete_number資訊，方便修正
-    #連接數據庫MySQL
-    conn = pymysql.connect(host="localhost",user="root",db="block",charset="utf8",port=None)
-    cur = conn.cursor() #取得指令操作變數
-    #執行插入方法，入的SQL語句
-    #格式化的作用是為了防止SQL注入，`的作用是為了防止關鍵詞被數據庫解析
-    cur.execute("DELETE FROM `article` WHERE `id`=%s",[delete_number])
+    # 連接數據庫MySQL
+    conn = pymysql.connect(host="localhost", user="root", db="block", charset="utf8", port=None)
+    cur = conn.cursor()  # 取得指令操作變數
+    # 執行插入方法，入的SQL語句
+    # 格式化的作用是為了防止SQL注入，`的作用是為了防止關鍵詞被數據庫解析
+    cur.execute("DELETE FROM `article` WHERE `id`=%s", [delete_number])
     conn.commit()
     conn.close()
 
-def change_message(modified_number,modified_name,modified_comment):
-    print(repr(modified_number)) #顯示delete_number資訊，方便修正
-    #連接數據庫MySQL
-    conn = pymysql.connect(host="localhost",user="root",db="block",charset="utf8",port=None)
-    cur = conn.cursor() #取得指令操作變數
-    #執行插入方法，入的SQL語句
-    #格式化的作用是為了防止SQL注入，`的作用是為了防止關鍵詞被數據庫解析
-    cur.execute("UPDATE `article` SET `name`=%s,`comment`=%s WHERE `id`=%s", [modified_name,modified_comment,modified_number])
+
+def change_message(modified_number, modified_name, modified_comment):
+    print(repr(modified_number))  # 顯示delete_number資訊，方便修正
+    # 連接數據庫MySQL
+    conn = pymysql.connect(host="localhost", user="root", db="block", charset="utf8", port=None)
+    cur = conn.cursor()  # 取得指令操作變數
+    # 執行插入方法，入的SQL語句
+    # 格式化的作用是為了防止SQL注入，`的作用是為了防止關鍵詞被數據庫解析
+    cur.execute("UPDATE `article` SET `name`=%s,`comment`=%s WHERE `id`=%s",
+                [modified_name, modified_comment, modified_number])
     conn.commit()
     conn.close()
 
+'''
+#原始版本，尚未加入換頁功能
 def load_data():
     conn = pymysql.connect(host="localhost", user="root", db="block", charset="utf8", port=None)
-    cur = conn.cursor() #取得指令操作變數
+    cur = conn.cursor()  # 取得指令操作變數
     cur.execute("SELECT * FROM `article`")
+    # "SELECT * FROM `article` LIMIT 10 OFFSET 5"
     list = cur.fetchall()
     conn.commit()
     conn.close()
     return list
+'''
 
-#使datetime對象更容易分辨的模板的過濾器
+def load_data_page(page=1):
+    conn = pymysql.connect(host="localhost", user="root", db="block", charset="utf8", port=None)
+    cur = conn.cursor()  # 取得指令操作變數
+    try:
+        offset = 10 * (page - 1)
+        LIMIT = 10
+        seq = "SELECT * FROM `article` LIMIT {LIMIT} OFFSET {offset}".format(LIMIT=LIMIT, offset=offset)
+        cur.execute(seq)
+    except:
+        cur.execute("SELECT * FROM `article` LIMIT 10 OFFSET 0")
+        # cur.execute("SELECT * FROM `article`")
+        # "SELECT * FROM `article` LIMIT 10 OFFSET 5"
+    finally:
+        list = cur.fetchall()
+        conn.commit()
+        conn.close()
+    return list
+
+
+# 使datetime對象更容易分辨的模板的過濾器
 @app.template_filter("datetime_fmt")
 def datetime_filter(dt):
     return dt.strftime(str('%Y/%m/%d %H:%M:%S'))
 
-#將換行符置換為br標籤的模板過濾器
+
+# 將換行符置換為br標籤的模板過濾器
 @app.template_filter("nl2br")
 def nl2br(s):
-    return escape(s).replace("\\n",Markup('<br>'))
+    return escape(s).replace("\\n", Markup('<br>'))
+
 
 # 伺服器啟動，執行flask項目
 if __name__ == "__main__":
