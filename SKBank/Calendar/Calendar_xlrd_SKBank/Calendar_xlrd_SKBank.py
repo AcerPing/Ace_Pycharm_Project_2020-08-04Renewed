1c97fd28d2f99c55af7d3d002f525ea612ac9ea1
AcerPing <acer0805@hotmail.com>
2021-11-04 15:50:25 +0800

    For SKBank 20211104 15:50

--- SKBank/Calendar/Calendar_xlrd_SKBank/Calendar_xlrd_SKBank.py
+++ SKBank/Calendar/Calendar_xlrd_SKBank/Calendar_xlrd_SKBank.py
@@ -214,14 +214,13 @@ def main():
     
     if ConvertNum(Excel_todo_list[i][key_column_id['營業']]) != 1: 
         print('不用上班')
-        Whether_Work = '不用上班'
     else: 
         print('Go to Work.')
-        Whether_Work = 'Go to Work.'
+    Whether_Work = ConvertNum(Excel_todo_list[i][key_column_id['營業']])
 
     # TODO: 1. 當天是否為上班日
     file=open("今天是否上班.txt",mode="w",encoding="utf-8") #開啟檔案
-    file.write(Whether_Work+"\n") #撰寫檔案
+    file.write(str(Whether_Work).strip()+"\n") #撰寫檔案
     file.close() #關閉檔案
     
     # TODO: 2. 上一次上班日是什麼時候?
@@ -236,7 +235,7 @@ def main():
         raise Exception('Excel Error', Custom_Err_Msg)
     
     file=open("上一個營業日.txt",mode="w",encoding="utf-8") #開啟檔案
-    file.write(PreviousWorkingDay+"\n") #撰寫檔案
+    file.write(str(PreviousWorkingDay).strip()+"\n") #撰寫檔案
     file.close() #關閉檔案
     
     # TODO: 3. 下一次上班日是什麼時候?
@@ -251,7 +250,7 @@ def main():
         raise Exception('Excel Error', Custom_Err_Msg)
     
     file=open("下一個營業日.txt",mode="w",encoding="utf-8") #開啟檔案
-    file.write(NextWorkingDay+"\n") #撰寫檔案
+    file.write(str(NextWorkingDay).strip()+"\n") #撰寫檔案
     file.close() #關閉檔案
 
 def check_result():
@@ -259,6 +258,20 @@ def check_result():
     檢查寫入的結果檔是否是否符合格式
     '''
     Custom_Err_Msg = ''
+
+    # TODO: 讀取 今天是否上班 檔案
+    with open("今天是否上班.txt",mode="r",encoding="utf-8") as file:
+        datum=file.read()
+    # print(datum)
+    o = re.fullmatch("^[0-9]{1}\n$", datum)
+    # print(o)
+    if o != None: print('True')
+    else:
+        Custom_Err_Msg += '今天是否上班 結果報表錯誤'
+        print('Excel Error')
+        print('Process Error, Process End. \n\n Error Details:\n' + Custom_Err_Msg)
+        raise Exception('結果報表 Error', Custom_Err_Msg)
+
     # TODO: 讀取 上一個營業日 檔案
     with open("上一個營業日.txt",mode="r",encoding="utf-8") as file:
         datum=file.read()
@@ -291,12 +304,12 @@ if __name__ == "__main__":
     # TODO: 執行，將結果寫入txt
     try:
         main()
-        time.sleep(2)
+        time.sleep(1)
         print('Done. 終わり！')
     except:
         error_message = sys.exc_info() #取得Call Stack
         with open("Error_Log.txt",mode="w",encoding="utf-8") as file:
-            file.write(str(error_message)+'\n')
+            file.write(str(error_message).strip()+'\n')
         input('Please "Key Enter" to continue the process.')
         sys.exit(1)
 
@@ -307,7 +320,7 @@ if __name__ == "__main__":
         error_message = sys.exc_info() #取得Call Stack
         print(error_message)
         with open("Error_Log.txt",mode="w",encoding="utf-8") as file:
-            file.write(str(error_message)+'\n')
+            file.write(str(error_message).strip()+'\n')
         input('Please "Key Enter" to continue the process.')
         sys.exit(1)
         
